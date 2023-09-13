797777685bd161b6339db7ddc6925934e5a7609a
GeniaKybalchych <117115542+GeniaKybalchych@users.noreply.github.com>
2023-09-13 11:51:52 -0400

    FireBase config

--- app/src/main/java/com/example/a17_tp_android/MainActivity.java
+++ app/src/main/java/com/example/a17_tp_android/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.a17_tp_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtenez une instance de la base de données avec l'URL correcte
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://a17-tp-android-default-rtdb.firebaseio.com/");
+
+        // Obtenez une référence à la "message" noeud
+        DatabaseReference myRef = database.getReference("message");
+
+        // Définir une valeur pour le noeud référencé
+        myRef.setValue("Hello, World!");
+
+        myRef.setValue("Hello, World!").addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("Firebase", "Données écrites avec succès !");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Firebase", "Erreur lors de l'écriture des données", e);
+            }
+        });
     }
 }
